0e3b614ec992212e9dd83f120217242218266dfc
Gmanlove <emmagmanc@gmail.com>
2023-10-29 01:35:34 +0100

    Completes the test

--- test/root.js
+++ test/root.js
@@ -5,11 +5,13 @@ const jsdom = require('jsdom');
 const path = require('path');
 
 before(function(done) {
+  this.timeout(10000);
   const babelResult = babel.transformFileSync(
     path.resolve(__dirname, '..', 'index.js'), {
       presets: ['es2015']
     }
   );
+  
 
   const html = path.resolve(__dirname, '..', 'index.html')
 
